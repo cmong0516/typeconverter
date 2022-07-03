17691156b483f45606a1a4bef7a6751234b63040
cmong0516 <cmong0516@gmail.com>
2022-07-03 20:34:35 +0900

    2022.07.03

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,29 @@
+package hello.typeconverter.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+// Servlet
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer intValue = Integer.valueOf(data);
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+// Spring
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
